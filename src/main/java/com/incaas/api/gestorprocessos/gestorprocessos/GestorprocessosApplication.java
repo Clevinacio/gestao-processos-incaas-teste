05021192ee592543a3a0a2820e27f062150eecbd
Cleverton Inácio <clev.inacio@gmail.com>
2025-06-04 11:31:48 -0300

    cria projeto

---
+++ src/main/java/com/incaas/api/gestorprocessos/gestorprocessos/GestorprocessosApplication.java
@@ -0,0 +1,13 @@
+package com.incaas.api.gestorprocessos.gestorprocessos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestorprocessosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestorprocessosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/incaas/api/gestorprocessos/gestorprocessos/GestorprocessosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.incaas.api.gestorprocessos.gestorprocessos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestorprocessosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
